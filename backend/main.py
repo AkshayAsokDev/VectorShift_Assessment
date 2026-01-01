0e19ce7d0cfa7bef26dfb8290154d8344822480d
Akshay Asok <akshayasokson@gmail.com>
2026-01-01 22:17:53 +0530

    POST for submit added, backend updated to receive json and return payload with dag check

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,69 @@
+# adding cors policuy 
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from typing import List
+
+from collections import defaultdict, deque
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # OK for assessment
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def read_root():
     return {'Ping': 'Pong'}
 
-@app.get('/pipelines/parse')
-def parse_pipeline(pipeline: str = Form(...)):
-    return {'status': 'parsed'}
+# Classes with BaseModel for validation
+
+class Pipeline(BaseModel):
+    nodes: List
+    edges: List
+
+
+
+@app.post('/pipelines/parse')
+def parse_pipeline(pipeline: Pipeline):
+    nodes = pipeline.nodes
+    edges = pipeline.edges
+    num_nodes = len(pipeline.nodes)
+    num_edges = len(pipeline.edges)
+
+    graph = defaultdict(list)
+    indegree = defaultdict(int)
+
+    # creating an adjaency matrix and indegree count
+    for edge in edges:
+        src = edge["source"]
+        tgt = edge["target"]
+        graph[src].append(tgt)
+        indegree[tgt] += 1
+
+    # get the 0 indeg
+    queue = deque([n["id"] for n in nodes if indegree[n["id"]] == 0])
+    visited = 0
+
+    # reduce indeg based on queue and append to queue if indeg is 0
+    while queue:
+        node = queue.popleft()
+        visited += 1
+        for nei in graph[node]:
+            indegree[nei] -= 1
+            if indegree[nei] == 0:
+                queue.append(nei)
+
+    is_dag = visited == num_nodes
+
+
+    return {
+        "num_nodes": num_nodes,
+        "num_edges": num_edges,
+        "is_dag": is_dag  # placeholder for now
+    }
--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -7,11 +7,36 @@ export const SubmitButton = () => {
 
     const {nodes, edges} = useStore(state => ({ nodes: state.nodes, edges: state.edges }), shallow);
     
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         console.log("nodes => ", nodes);
         console.log("edges => ", edges);
 
         //post to backend later
+        const payload = {
+            nodes,
+            edges
+        }
+
+        try {
+            const response = await fetch("http://localhost:8000/pipelines/parse", {
+                    method: "POST",
+                    headers: {
+                    "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(payload),
+            })
+
+            const data = await response.json();
+            console.log("Backend response:", data);
+
+            alert(
+                `Nodes: ${data.num_nodes}\nEdges: ${data.num_edges}\nDAG: ${data.is_dag}`
+            );
+        }
+        catch(error){
+            console.error("error while sending to backend >> ", error);
+            alert("Failed to send to pipeline backend");
+        }
     }
 
     return (
